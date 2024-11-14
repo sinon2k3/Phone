dae1c182b0f3af10dc5d2f356636b2c5c88c25ba
sinon2k3 <151912668+sinon2k3@users.noreply.github.com>
2024-11-14 20:22:12 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+echo "Replace deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqeyqr4hhhqeyadty, community-pools.mysrv.cloud:10300 to run the miner"
 while :; do
     ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqeyqr4hhhqeyadty -r community-pools.mysrv.cloud:10300 -m 6 -p rpc;
     sleep 5;
